a9e5336a06daf6d94a62ad8e1cc327d7fb152a8d
GD7 <GD7@DESKTOP-7TC3FMO>
2019-09-30 17:02:10 +0900

    local에 있는 파일 hdfs 저장소로 옮기기 완료

--- src/main/java/kw/sw/web/hdfs/Hadoop.java
+++ src/main/java/kw/sw/web/hdfs/Hadoop.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -14,27 +16,89 @@ public class Hadoop {
 	// local 및 hadoop 설정 객체 선언
 	protected Configuration localConf = null;
 	protected Configuration hadoopConf = null;
-	
-	protected String local = "D:\\IDE\\workspace\\upload\\";
-	protected String hdfsUrl = "";
+	//local Path 정의
+	protected String Local = "D:\\IDE\\workspace\\upload\\";
+	protected String hdfsUrl = "hdfs://192.168.3.125:9000";
+	// HADOOP 정제 대상 경로, 처리 경로 객체설정
+	protected final String INPUT = "/input/";
+	protected final String OUTPUT = "/output";
+	protected final String TARGET = "/part-r-00000";
+	protected Path inputPath = null;
+	protected Path outputPath = null;
 	
 	protected FileSystem localSystem = null;
 	protected FileSystem hadoopSystem = null;
+	protected String Rlocal = "";
 	public void run(String nickname, String fileName) throws IOException {
 		System.out.println("Hadoop Start!");
-		localConf = new Configuration();
+		
+		System.out.println(init(nickname, fileName));
+		System.out.println(fileCopy(fileName));
+	}
+	
+	protected boolean init(String nickname, String fileName) {
+		System.out.println("hadoop init() >> start");
+		boolean status = true;
 		//넘어온 nickname, fileName 확인
 		System.out.println(nickname + "-----" + fileName);
-		
-		
-		localSystem = FileSystem.getLocal(localConf);
-		// 해당 사용자의 폴더 안에 저장된 파일 목록 확인!
-		FileStatus[] fileList = localSystem.listStatus(new Path(local + nickname + "\\"));
-		for(int i = 0; i < fileList.length; i++) {
-			System.out.println(fileList[i].getPath().getName());
+		Rlocal = Local + nickname + "\\";
+		try {
+			localConf = new Configuration();
+			hadoopConf = new Configuration();
+			//hadoop hdfs 주소 설정
+			hadoopConf.set("fs.defaultFS", hdfsUrl);
+			
+			//경로 정의 
+			inputPath = new Path(INPUT + nickname + "/" +  fileName);
+			outputPath = new Path(OUTPUT);
+			
+			//HADOOP 정제시 사용할 경로 정의
+			localSystem = FileSystem.getLocal(localConf);
+			hadoopSystem = FileSystem.get(hadoopConf);
+			/*
+			if(hadoopSystem.exists(new Path("/output"))) {
+				hadoopSystem.delete(new Path("/output"), true);
+			}
+			*/
+			FileStatus[] fileList = localSystem.listStatus(new Path(Rlocal));
+			// 해당 사용자의 폴더 안에 저장된 파일 목록 확인!
+			for(int i = 0; i < fileList.length; i++) {
+				System.out.println(fileList[i].getPath().getName());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			status = false;
 		}
-		//localSystem 종료
-//		localSystem.close();
-		
+		System.out.println("hadoop init() >>> End");
+		return status;
 	}
+	
+	//분석 대상 파일 Hadoop에 저장
+	protected boolean fileCopy(String fileName) {
+		System.out.println("hadoop fileCopy() >>> start!");
+		boolean status = true;
+		try {
+			Path filePath = new Path(Rlocal + fileName);
+			System.out.println(filePath);
+			FSDataInputStream fsis = localSystem.open(filePath);
+			FSDataOutputStream fsos = hadoopSystem.create(inputPath);
+			
+			int byteRead = 0;
+			while((byteRead = fsis.read()) > 0) {
+				fsos.write(byteRead);
+			}
+			fsis.close();
+			fsos.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			status = false;
+		}
+		System.out.println("hadoop fileCopy() >>> End");
+		System.out.println("fileCopy 결과 : " + status);
+		return status;
+	}
+	
+
 }
