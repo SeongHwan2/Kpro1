d31d380e8f97b4c3781b99fa91ec8fd5a055f293
GD7 <GD7@DESKTOP-7TC3FMO>
2019-10-10 18:22:24 +0900

    예외처리 전

--- src/main/java/kw/sw/web/hdfs/Map1.java
+++ src/main/java/kw/sw/web/hdfs/Map1.java
@@ -31,12 +31,11 @@ public class Map1 extends Mapper<LongWritable, Text, Text, Text> {
 		if(values.length > 1) {
 			//출력키에 넣을 문자열 변수 선언
 			strKey = values[0];
-			if(strKey.indexOf("[출처]") == -1) {
-				strVal1 = values[1];
-				strValC = "";
-				strVal2 = values[2];
-				length = strKey.length();
-			}
+			strVal1 = values[1];
+			strValC = "";
+			strVal2 = values[2];
+			length = strKey.length();
+			
 			
 		} else {
 			return;
